6f5ba55a574ecdb613a2253345dd8de12d761963
Alex <alex0692pash@mail.ru>
2020-11-04 17:39:11 +0300

    Init 002

---
+++ 002_Tic-tac-toe/cmake-build-debug/CMakeFiles/002_Tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "002_Tic_tac_toe.exe"
+  "002_Tic_tac_toe.exe.manifest"
+  "002_Tic_tac_toe.pdb"
+  "CMakeFiles/002_Tic_tac_toe.dir/main.cpp.obj"
+  "lib002_Tic_tac_toe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/002_Tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
