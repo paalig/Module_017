2598f1ba8038f4b41ff29eef9f2164ba92bb96e9
Alex <alex0692pash@mail.ru>
2020-11-04 17:37:36 +0300

    Init 001

---
+++ 001_BanquetTable/cmake-build-debug/CMakeFiles/001_BanquetTable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "001_BanquetTable.exe"
+  "001_BanquetTable.exe.manifest"
+  "001_BanquetTable.pdb"
+  "CMakeFiles/001_BanquetTable.dir/main.cpp.obj"
+  "lib001_BanquetTable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_BanquetTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 001_BanquetTable/cmake-build-debug/CMakeFiles/001_BanquetTable.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 001_BanquetTable.
+# This may be replaced when dependencies are built.
