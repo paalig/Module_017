5e1aca15e0d699d8fe6f0f9a092def56e22e91e4
Alex <alex0692pash@mail.ru>
2020-11-15 00:25:58 +0300

    003 done

--- 003_Snake/main.cpp
+++ 003_Snake/main.cpp
@@ -15,6 +15,18 @@
  */
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int snake[5][5];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            snake[i][j] = j + i * 5 + (i % 2) * (2 - j) * 2;
+
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            std::cout << snake[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
