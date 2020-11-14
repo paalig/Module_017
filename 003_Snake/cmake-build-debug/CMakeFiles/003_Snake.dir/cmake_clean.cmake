4d3b244cdc869c5a3a821cb2ac2beeea6ceca1bd
Alex <alex0692pash@mail.ru>
2020-11-14 22:42:17 +0300

    Init 003

---
+++ 003_Snake/cmake-build-debug/CMakeFiles/003_Snake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "003_Snake.exe"
+  "003_Snake.exe.manifest"
+  "003_Snake.pdb"
+  "CMakeFiles/003_Snake.dir/main.cpp.obj"
+  "lib003_Snake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/003_Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
