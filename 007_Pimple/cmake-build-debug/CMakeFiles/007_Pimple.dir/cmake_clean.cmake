284c766fd89314248477114323adba1b851eb38f
Alex <alex0692pash@mail.ru>
2020-11-18 00:21:06 +0300

    Init 007

---
+++ 007_Pimple/cmake-build-debug/CMakeFiles/007_Pimple.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "007_Pimple.exe"
+  "007_Pimple.exe.manifest"
+  "007_Pimple.pdb"
+  "CMakeFiles/007_Pimple.dir/main.cpp.obj"
+  "lib007_Pimple.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/007_Pimple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 007_Pimple/cmake-build-debug/CMakeFiles/007_Pimple.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 007_Pimple.
+# This may be replaced when dependencies are built.
