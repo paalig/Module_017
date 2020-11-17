f604ab8e143bae76ac5d113aec18ebe6a09f76b8
Alex <alex0692pash@mail.ru>
2020-11-17 22:46:41 +0300

    Init 006, variables, input and print methods

---
+++ 006_MatrixMultiplyVector/cmake-build-debug/CMakeFiles/006_MatrixMultiplyVector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "006_MatrixMultiplyVector.exe"
+  "006_MatrixMultiplyVector.exe.manifest"
+  "006_MatrixMultiplyVector.pdb"
+  "CMakeFiles/006_MatrixMultiplyVector.dir/main.cpp.obj"
+  "lib006_MatrixMultiplyVector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/006_MatrixMultiplyVector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
