e2e045061b5dee7b60f1a5ef91bfcf06d2240756
Alex <alex0692pash@mail.ru>
2020-11-17 22:09:24 +0300

    Init 005

---
+++ 005_DiagonalMatrix/cmake-build-debug/CMakeFiles/005_DiagonalMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "005_DiagonalMatrix.exe"
+  "005_DiagonalMatrix.exe.manifest"
+  "005_DiagonalMatrix.pdb"
+  "CMakeFiles/005_DiagonalMatrix.dir/main.cpp.obj"
+  "lib005_DiagonalMatrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/005_DiagonalMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 005_DiagonalMatrix/cmake-build-debug/CMakeFiles/005_DiagonalMatrix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 005_DiagonalMatrix.
+# This may be replaced when dependencies are built.
