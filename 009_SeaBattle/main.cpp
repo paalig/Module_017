a376bc050a3dc13ea3ec913dbecca8b4edb92142
Alex <alex0692pash@mail.ru>
2020-11-22 00:21:42 +0300

    009 done

--- 009_SeaBattle/main.cpp
+++ 009_SeaBattle/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 /*
  * Требуется реализовать упрощённую игру в Морской бой. Игровое поле размером 10 на 10 клеток.
  * Участвует двое игроков. В арсенале каждого из них: 4 маленьких кораблика размером в одну клетку,
@@ -25,13 +24,20 @@
  * Игра заканчивается тогда, когда все корабли одной из сторон будут полностью уничтожены.
  * Как только это произойдёт в консоль выводится информация с номером игрока, который победил.
  */
-bool CheckLocation(int field[10][10], int start1, int end1, int start2, int end2) {
+bool CheckLocation(char field[10][10], int start1, int end1, int start2, int end2) {
     for (int i = std::min(start1, start2); i <= std::max(start1, start2); i++) {
         for (int j = std::min(end1, end2); j <= std::max(end1, end2); j++) {
-            if (field[i][j] = ' ') {
+            if (field[i][j] == ' ') {
                 return false;
             }
         }
+    }
+    return true;
+}
+
+bool CheckLocationTiny(char field[10][10], int start1, int end1) {
+    if (field[start1][end1] == ' ') {
+        return false;
     }
     return true;
 }
@@ -44,111 +50,76 @@ bool CheckCoordinate(int a) {
     }
 }
 
-int InstallShips(int arr[10][10]) {
-
+char Arrange(char arr[10][10], int count, std::string type, int length) {
     int start1, end1, start2, end2;
 
-    int tiny = 4;
-    int small = 3;
-    int big = 2;
-    int huge = 1;
-    while (tiny > 0) {
+    while (count > 0) {
+        std::cout << count << " ships left" << std::endl;
+        std::cout << "Input first coordinate of " << type << " ship:";
+        std::cin >> start1 >> end1;
+        std::cout << "Input second coordinate of " << type << " ship:";
+        std::cin >> start2 >> end2;
+        while (!CheckCoordinate(start1) || !CheckCoordinate(end1) || !CheckCoordinate(start2)
+               || !CheckCoordinate(end2) || (abs(start1 - start2) > 0 && abs(end1 - end2) > 0)
+               || (abs(start1 - start2) != (length - 1) && abs(end1 - end2) != (length - 1))) {
+            std::cout << "Wrong coordinate input. Try again" << std::endl;
+            std::cout << "Input first coordinate of " << type << " ship:";
+            std::cin >> start1 >> end1;
+            std::cout << "Input second coordinate of " << type << " ship:";
+            std::cin >> start2 >> end2;
+        }
+        if (CheckLocation(arr, start1, end1, start2, end2)) {
+            for (int i = std::min(start1, start2); i <= std::max(start1, start2); i++) {
+                for (int j = std::min(end1, end2); j <= std::max(end1, end2); j++) {
+                    arr[i][j] = ' ';
+                }
+            }
+            count--;
+        } else {
+            std::cout << "Position of the ship is already filled. Try again" << std::endl;
+        }
+    }
+    return arr[10][10];
+}
 
-        std::cout << tiny << " ships left" << std::endl;
+char InstallShips(char arr[10][10]) {
+
+    int tinyCount = 4;
+    int smallCount = 3;
+    int bigCount = 2;
+    int hugeCount = 1;
+
+    int start1, end1;
+    std::cout << "Install tiny ships:" << std::endl;
+    while (tinyCount > 0) {
+        std::cout << tinyCount << " ships left" << std::endl;
         std::cout << "Input coordinate of tiny ship:";
         std::cin >> start1 >> end1;
         while (!CheckCoordinate(start1) || !CheckCoordinate(end1)) {
-            std::cout << "Wrong input. Try again";
+            std::cout << "Wrong coordinate input. Try again";
             std::cin >> start1 >> end1;
         }
-        if (CheckLocation(arr, start1, end1, start2, end2)) {
+        if (CheckLocationTiny(arr, start1, end1)) {
             arr[start1][end1] = ' ';
-            tiny--;
+            tinyCount--;
         } else {
             std::cout << "Position of the ship is already filled. Try again" << std::endl;
         }
     }
-    while (small > 0) {
-        std::cout << small << " ships left" << std::endl;
-        std::cout << "Input first coordinate of small ship:";
-        std::cin >> start1 >> end1;
-        std::cout << "Input second coordinate of small ship:";
-        std::cin >> start2 >> end2;
-        while (!CheckCoordinate(start1) || !CheckCoordinate(end1) || !CheckCoordinate(start2)
-                || !CheckCoordinate(end2) || (abs(start1 - start2) > 0 && abs(end1 - end2) > 0)
-                || abs(start1 - start2) != 2 || abs(end1 - end2) != 2) {
-            std::cout << "Wrong input. Try again" << std::endl;
-            std::cout << "Input first coordinate of small ship:";
-            std::cin >> start1 >> end1;
-            std::cout << "Input second coordinate of small ship:";
-            std::cin >> start2 >> end2;
-        }
-        if (CheckLocation(arr, start1, end1, start2, end2)) {
-            for (int i = std::min(start1, start2); i <= std::max(start1, start2); i++) {
-                for (int j = std::min(end1, end2); j <= std::max(end1, end2); j++) {
-                    arr[i][j] = ' ';
-                }
-            }
-            small--;
-        } else {
-            std::cout << "Position of the ship is already filled. Try again" << std::endl;
-        }
-    }
-    while (big > 0) {
-        std::cout << big << " ships left" << std::endl;
-        std::cout << "Input first coordinate of big ship:";
-        std::cin >> start1 >> end1;
-        std::cout << "Input second coordinate of big ship:";
-        std::cin >> start2 >> end2;
-        while (!CheckCoordinate(start1) || !CheckCoordinate(end1) || !CheckCoordinate(start2)
-               || !CheckCoordinate(end2) || (abs(start1 - start2) > 0 && abs(end1 - end2) > 0)
-               || abs(start1 - start2) != 3 || abs(end1 - end2) != 3) {
-            std::cout << "Wrong input. Try again" << std::endl;
-            std::cout << "Input first coordinate of small ship:";
-            std::cin >> start1 >> end1;
-            std::cout << "Input second coordinate of small ship:";
-            std::cin >> start2 >> end2;
-        }
-        if (CheckLocation(arr, start1, end1, start2, end2)) {
-            for (int i = std::min(start1, start2); i <= std::max(start1, start2); i++) {
-                for (int j = std::min(end1, end2); j <= std::max(end1, end2); j++) {
-                    arr[i][j] = ' ';
-                }
-            }
-            big--;
-        } else {
-            std::cout << "Position of the ship is already filled. Try again" << std::endl;
-        }
-    }
-    while (huge > 0) {
-        std::cout << huge << " ships left" << std::endl;
-        std::cout << "Input first coordinate of big ship:";
-        std::cin >> start1 >> end1;
-        std::cout << "Input second coordinate of big ship:";
-        std::cin >> start2 >> end2;
-        while (!CheckCoordinate(start1) || !CheckCoordinate(end1) || !CheckCoordinate(start2)
-               || !CheckCoordinate(end2) || (abs(start1 - start2) > 0 && abs(end1 - end2) > 0)
-               || abs(start1 - start2) != 4 || abs(end1 - end2) != 4) {
-            std::cout << "Wrong input. Try again" << std::endl;
-            std::cout << "Input first coordinate of small ship:";
-            std::cin >> start1 >> end1;
-            std::cout << "Input second coordinate of small ship:";
-            std::cin >> start2 >> end2;
-        }
-        if (CheckLocation(arr, start1, end1, start2, end2)) {
-            for (int i = std::min(start1, start2); i <= std::max(start1, start2); i++) {
-                for (int j = std::min(end1, end2); j <= std::max(end1, end2); j++) {
-                    arr[i][j] = ' ';
-                }
-            }
-            huge--;
-        } else {
-            std::cout << "Position of the ship is already filled. Try again" << std::endl;
-        }
-    }
+    std::cout << std::endl;
+    std::cout << "Install small ships:" << std::endl;
+    Arrange(arr, smallCount, "small", 2);
+    std::cout << std::endl;
+    std::cout << "Install big ships:" << std::endl;
+    Arrange(arr, bigCount, "big", 3);
+    std::cout << std::endl;
+    std::cout << "Install huge ships:" << std::endl;
+    Arrange(arr, hugeCount, "huge", 4);
+    std::cout << std::endl;
+    return arr[10][10];
 }
 
-int InitField(int arr[10][10]) {
+char InitField(char arr[10][10]) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             arr[i][j] = '0';
@@ -157,37 +128,98 @@ int InitField(int arr[10][10]) {
     return arr[10][10];
 }
 
-int main() {
-    int firstPlayer[10][10];
-    int secondPlayer[10][10];
-    int firstField[10][10];
-    int secondField[10][10];
+bool CheckWin(char arr[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (arr[i][j] == ' ') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
-    //заполнить поля
+bool Shot(char arr[10][10], int a, int b) {
+    if (arr[a][b] == ' ') {
+        return true;
+    }
+    return false;
+}
+
+std::string ChangePlayer(std::string player) {
+    if (player == "Player 1") {
+        player = "Player 2";
+    } else {
+        player = "Player 1";
+    }
+    return player;
+}
+
+void PrintField(char arr[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            std::cout << arr[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    char firstPlayer[10][10];
+    char secondPlayer[10][10];
+    char firstField[10][10];
+    char secondField[10][10];
+
     InitField(firstPlayer);
     InitField(secondPlayer);
-    firstField[10][10] = firstPlayer[10][10];
-    secondField[10][10] = secondPlayer[10][10];
+    InitField(firstField);
+    InitField(secondField);
 
-    //расставить корабли
-    std::cout << "Player 1, arrange your ships!" << std::endl;
+    std::cout << "Player 1:" << std::endl;
     InstallShips(firstPlayer);
     std::cout << "First player finished" << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "Player 2, arrange your ships!" << std::endl;
+    std::cout << "Player 2:" << std::endl;
     InstallShips(secondPlayer);
     std::cout << "Second player finished" << std::endl;
+    std::cout << std::endl;
 
-    //выстрел1
-
-    //проверка2
-
-    //выстрел2
-
-    //проверка1
-
-    //инфо о победе
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string currentPlayer = "Player 1";
+    while (CheckWin(firstPlayer) && CheckWin(secondPlayer)) {
+        int a, b;
+        std::cout << currentPlayer << std::endl;
+        std::cout << "Make your shot!";
+        std::cin >> a >> b;
+        while (!CheckCoordinate(a) || !CheckCoordinate(b)) {
+            std::cout << "Wrong coordinate. Try again";
+            std::cin >> a >> b;
+        }
+        if (currentPlayer == "Player 1") {
+            if (Shot(secondPlayer, a, b)) {
+                secondPlayer[a][b] = '0';
+                secondField[a][b] = ' ';
+                std::cout << "Hit!" << std::endl;
+            } else {
+                secondField[a][b] = '*';
+                std::cout << "Past!" << std::endl;
+            }
+            PrintField(secondField);
+            std::cout << std::endl;
+        } else {
+            if (Shot(firstPlayer, a, b)) {
+                firstPlayer[a][b] = '0';
+                firstField[a][b] = ' ';
+                std::cout << "Hit!" << std::endl;
+            } else {
+                firstField[a][b] = '*';
+                std::cout << "Past!" << std::endl;
+            }
+            PrintField(firstField);
+            std::cout << std::endl;
+        }
+        currentPlayer = ChangePlayer(currentPlayer);
+    }
+    currentPlayer = ChangePlayer(currentPlayer);
+    std::cout << currentPlayer << " won!" << std::endl;
 }
